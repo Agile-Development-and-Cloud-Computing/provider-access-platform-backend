748df8d0f30a825baa267e0a088e5ffb67619a2e
MandarGK <mandarkale5@gmail.com>
2024-12-18 18:53:17 +0100

    LoginRequest Model Implementated

--- backend/backend/src/main/java/com/fuas/providers_access_platform/controller/LoginController.java
+++ backend/backend/src/main/java/com/fuas/providers_access_platform/controller/LoginController.java
@@ -1,15 +1,13 @@
 package com.fuas.providers_access_platform.controller;
 
+import com.fuas.providers_access_platform.model.LoginRequest;
 import com.fuas.providers_access_platform.model.User;
 import com.fuas.providers_access_platform.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,26 +18,13 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    /*
-    @PostMapping("/login")
-    public ResponseEntity<?> processLogin(@RequestParam("username") String username,
-                                          @RequestParam("password") String password) {
-        User user = userService.authenticate(username, password);
-        if (user != null) {
-            // Successful login
-            return ResponseEntity.ok().body("{ \"message\": \"Login is successful\" }");
-        } else {
-            // Failed login
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{ \"message\": \"Invalid username or password\" }");
-        }
-    }
-    */
+
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> processLogin(@RequestParam("username") String username,
-                                                            @RequestParam("password") String password) {
+    public ResponseEntity<Map<String, String>> processLogin(@RequestBody LoginRequest loginRequest) {
+        System.out.println("LoginRequest: " + loginRequest);
         Map<String, String> response = new HashMap<>();
-        User user = loginService.authenticate(username, password);
+        User user = loginService.authenticate(loginRequest.getUsername(), loginRequest.getPassword());
         if (user != null) {
             response.put("message", "Login is successful");
             return ResponseEntity.ok(response);
@@ -48,6 +33,7 @@ public class LoginController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
     }
+
     @GetMapping("/logout")
     public ResponseEntity<?> logout() {
         return ResponseEntity.ok().body("{ \"message\": \"Successfully logged out\" }");
