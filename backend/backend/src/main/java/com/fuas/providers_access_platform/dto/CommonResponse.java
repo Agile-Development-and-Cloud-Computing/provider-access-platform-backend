e8f765ba3740ac337afa684fc3b059297cfd1ef6
MandarGK <mandarkale5@gmail.com>
2024-12-19 01:11:23 +0100

    Timestamp added in JSON Reponse

--- backend/backend/src/main/java/com/fuas/providers_access_platform/dto/CommonResponse.java
+++ backend/backend/src/main/java/com/fuas/providers_access_platform/dto/CommonResponse.java
@@ -1,17 +1,34 @@
 package com.fuas.providers_access_platform.dto;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class CommonResponse<T> {
     private boolean success;
     private String message;
+    private String timestamp;
     private T data;
+
 
     // Constructors
     public CommonResponse() {}
 
-    public CommonResponse(boolean success, String message, T data) {
+    public CommonResponse(boolean success, String message,T data ) {
         this.success = success;
         this.message = message;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        this.timestamp = LocalDateTime.now().format(formatter);
         this.data = data;
+
+
+
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
     }
 
     // Getters and Setters
@@ -44,6 +61,7 @@ public class CommonResponse<T> {
         return "CommonResponse{" +
                 "success=" + success +
                 ", message='" + message + '\'' +
+                ", timestamp=" + timestamp +
                 ", data=" + data +
                 '}';
     }
